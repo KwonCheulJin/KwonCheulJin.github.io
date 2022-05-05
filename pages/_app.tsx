99859fe4c01c19f13be2156a747b1d8b180eafbb
KwonCheulJin <chkftm12@gmail.com>
2022-05-05 15:29:09 +0900

    index clean

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/react-in-jsx-scope */
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
