98fec6018043c4e939f05fc4a10c055c3d7442e2
KwonCheulJin <chkftm12@gmail.com>
2022-05-22 23:28:51 +0900

    add webpack config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true,
-  optimizeFonts: false,
-  images: {
-    loader: 'imgix',
-    path: '',
-    disableStaticImages: true,
+const withImages = require('next-images');
+module.exports = withImages({
+  webpack(config, options) {
+    return config;
   },
-};
-
-module.exports = nextConfig;
+});
